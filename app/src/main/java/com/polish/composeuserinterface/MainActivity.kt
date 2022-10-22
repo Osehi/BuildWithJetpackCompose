88ae2f82745eeabcbe5ca66244186de27cc73c25
Osehi <osehiaseehilen@gmail.com>
2022-10-22 16:04:17 +0100

    Added birthday-greeting ui

--- app/src/main/java/com/polish/composeuserinterface/MainActivity.kt
+++ app/src/main/java/com/polish/composeuserinterface/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,7 +55,7 @@ fun GreetSamWithImage(name: String, sender: String) {
             painter = image,
             contentDescription = null,
             modifier = Modifier.fillMaxHeight().fillMaxWidth(),
-            contentScale = ContentScale.FillHeight,
+            contentScale = ContentScale.Crop,
 
         )
         Column {
@@ -61,14 +63,16 @@ fun GreetSamWithImage(name: String, sender: String) {
                 text = "Happy Birthday $name!!",
                 fontSize = 36.sp,
                 color = Color.White,
-                modifier = Modifier.padding(all = 4.dp),
+                modifier = Modifier.fillMaxWidth().wrapContentWidth(align = Alignment.Start)
+                    .padding(all = 4.dp),
                 fontWeight = FontWeight.Bold
             )
             Text(
                 text = "- from $sender",
                 fontSize = 18.sp,
                 color = Color.Blue,
-                modifier = Modifier.padding(all = 4.dp),
+                modifier = Modifier.fillMaxWidth().wrapContentWidth(align = Alignment.CenterHorizontally)
+                    .padding(all = 4.dp),
                 fontWeight = FontWeight.Bold
             )
         }
@@ -84,6 +88,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeUserInterfaceTheme {
-        GreetSamWithImage(name = "Sam", sender = "Emma")
+        GreetSamWithImage(name = stringResource(R.string.celebrant_name), sender = "Emma")
     }
 }
